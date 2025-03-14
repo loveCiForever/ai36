e26869cb16c70c6447aa1ca1457d9bec65f2d22d
Nguyễn Phúc Toàn <ngphuc_toan@tutanota.com>
2025-03-14 10:25:57 +0700

    Add TypeComponent and fix some wordings

--- task2/components.py
+++ task2/components.py
@@ -4,18 +4,29 @@ class Component:
 
 
 class PositionComponent(Component):
-    """Handles positioning of entities."""
-    def __init__(self, x: int=0, y: int=0):
+    """Handles positioning of the entity."""
+
+    def __init__(self, x: int = 0, y: int = 0):
         self.x, self.y = x, y
 
 
 class MoveableComponent(Component):
-    """Allows entities to be moved."""
+    """Allows the entity to move."""
+
     def __init__(self):
         self.dx, self.dy = 0, 0
 
 
 class ConsumableComponent(Component):
-    """Allow entities to be consumed."""
-    def __init__(self, points: int=1):
+    """Allows the entity to be consumed."""
+
+    def __init__(self, points: int = 1):
         self.points = points
+
+
+class TypeComponent(Component):
+    """Assigns an ASCII or Unicode character to the entity."""
+
+    def __init__(self, char: str):
+        assert len(char) == 1
+        self.char = char
